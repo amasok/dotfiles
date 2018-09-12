4e13b65a2366240e86da5ae8d3181f05604a00e4
kosuke.takeda <k_takeda@serori.co.jp>
2018-09-12 16:55:30 +0900

    phpenv編集

--- scripts/phpenv.sh
+++ scripts/phpenv.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/zsh
 
 sudo apt-get install libcurl4-gnutls-dev libcurl4-nss-dev libcurl4-gnutls-dev libpng3 libjpeg-dev re2c libxml2-dev openssl-dev libtidy-dev libxslt-dev libmcrypt-dev  libreadline-dev
+
+sudo apt-get install -y libxml2 libxml2-dev re2c autoconf \
+  libssl-dev bison build-essential libpng-dev libxslt-dev\
+  libcurl4-openssl-dev libmcrypt-dev libreadline-dev \
+  libjpeg8 libjpeg8-dev zlib1g-dev libtidy-dev libbz2-dev
+
 git clone https://github.com/CHH/phpenv.git ~/phpenv
 cd ~/phpenv/bin
 ./phpenv-install.sh
