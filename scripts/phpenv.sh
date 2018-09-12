2e9c01e0fd419a8a8a49316f1d317442b433434b
kosuke.takeda <k_takeda@serori.co.jp>
2018-09-12 15:47:16 +0900

    phpenv編集

--- scripts/phpenv.sh
+++ scripts/phpenv.sh
@@ -4,8 +4,6 @@ git clone https://github.com/CHH/phpenv.git ~/phpenv
 cd ~/phpenv/bin
 ./phpenv-install.sh
 
-sudo yum -y install epel-release
-sudo yum -y install gcc libxml2 libxml2-devel libcurl libcurl-devel libpng libpng-devel libmcrypt libmcrypt-devel libtidy libtidy-devel libxslt libxslt-devel openssl-devel bison libjpeg-turbo-devel readline-devel autoconf
 git clone https://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
 
 export PATH="$HOME/.phpenv/bin:$PATH"
