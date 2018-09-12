f5d2d887b9649c3fc49f9ad50f611e8ba3026cf6
kosuke.takeda <k_takeda@serori.co.jp>
2018-09-12 11:43:01 +0900

    zshrc修正

--- scripts/zshenv.sh
+++ scripts/zshenv.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/zplug/zplug ~/.zplug
 export ZPLUG_HOME=~/.zplug
 mkdir -p ~/.cache/shell/
-# source ~/.zshrc
-zsh -ic 'zplug install'
+# # source ~/.zshrc
+# zsh -ic 'zplug install'
