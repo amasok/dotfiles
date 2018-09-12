df19af5d3845ba1e54a928f07d20dc882c40bd9c
kosuke.takeda <k_takeda@serori.co.jp>
2018-09-12 15:49:49 +0900

    phpenv編集

--- scripts/phpenv.sh
+++ scripts/phpenv.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/zsh
 
+sudo apt-get install libcurl4-gnutls-dev libcurl4-nss-dev libcurl4-gnutls-dev libpng3 libjpeg-dev re2c libxml2-dev openssl-dev libtidy-dev libxslt-dev libmcrypt-dev  libreadline-dev
 git clone https://github.com/CHH/phpenv.git ~/phpenv
 cd ~/phpenv/bin
 ./phpenv-install.sh
